68d1aaf6a6726f5cc5c8596d01ac0fce1f9e3ee1
Izza <icg417@gmail.com>
2016-10-17 13:01:55 -0400

    added methods

--- main.rb
+++ main.rb
@@ -2,6 +2,28 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './models'
 
-
-
 set :database, "sqlite3:class.sqlite3"
+
+
+
+
+def all
+   command = `sqlite3 class.db "SELECT * FROM students"`
+   sql_results_parser command
+ end
+
+ def sql_results_parser(command)
+   #some code to parse what gets returned from the SQL command
+ end
+
+
+ def last
+ 	command = `sqlite3 class.db "SELECT * FROM Table students BY ID DESC LIMIT 1;"`
+ 	puts command
+ end
+
+ def first
+ 	command = `sqlite3 class.db "SELECT FROM Table students BY ID LIMIT 1;"`
+ 	puts command
+ end
+
